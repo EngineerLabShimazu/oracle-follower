ae8018a2ddf79bf77d67be065bdf207c5716a300
emu_yuu <mhk2.747.vclv@gmail.com>
2020-03-17 20:49:43 +0900

    今日はここまで

--- lambda/backend/oracle_follower.py
+++ lambda/backend/oracle_follower.py
@@ -8,9 +8,12 @@ def main(info):
     loaded_info = json.loads(info)
 
     _user = user.get_user(alexa_user_id=loaded_info['alexa_user_id'])
+    response_text = [hero.message()]
+
     if _user.is_first_launch_today:
         _user.increase_follower()
+        response_text.append(hero.increase_follower(_user.follower_increase,
+                                                    _user.follower_total_amount))
 
-    response_text = hero.message()
-    response = {"response_text": response_text}
+    response = {"response_text": "。".join(response_text)}
     return json.dumps(response)
