c686b6023c23ae94ef96973759d86d1501037b37
GenkiTech <shimazu.g.r@gmail.com>
2020-03-22 22:30:23 +0900

    feat: card-34304179で不必要なwhen option を削除

--- lambda/backend/dynamo_ctl.py
+++ lambda/backend/dynamo_ctl.py
@@ -13,17 +13,11 @@ class DynamoCtl:
         _attr = _get_attr(alexa_user_id)
         if not _attr:
             _attr = {
-                'when': {
-                    iso_formatted_date_today: {
-                        'follower_increase': 0,
-                        'follower_total_amount': 0,
-                        'destination': '',
-                        }
-                    },
                 'last_launch_date': '',
-                'follower_total_amount': 0
+                'follower_total_amount': 0,
+                'follower_increase': 0
                 }
-        self.attr = _attr
+        self._attr = _attr
 
     def __enter__(self):
         return self
@@ -31,12 +25,17 @@ class DynamoCtl:
     def __exit__(self, exc_type, exc_val, exc_tb):
         save_attr(self.alexa_user_id, self.attr)
 
-    def set_attr(self, today_attr: dict):
+    @property
+    def attr(self):
+        return self._attr
+
+    @attr.setter
+    def attr(self, value):
         d = {}
-        for k, v in today_attr.items():
+        for k, v in value.items():
             if v:
                 d[k] = v
-        self.attr['when'][iso_formatted_date_today] = d
+        self._attr = d
 
 
 def _serialize_attribute(attributes):
@@ -47,15 +46,9 @@ def _get_attr(user_id):
     """
     :return:
         attr = {
-            'when': {
-                'YYYY-mm-dd': {
-                    'follower_increase': -1,
-                    'follower_total_amount': -1,
-                    'destination': 'village_a',
-                    }
-                },
             'last_launch_date': 'YYYY-mm-dd',
-            'follower_total_amount': -1
+            'follower_total_amount': -1,
+            'follower_increase': -1
             }
     """
     item = dynamo.get_item(TableName='funDom-oracle-follower-user',
--- lambda/backend/user.py
+++ lambda/backend/user.py
@@ -2,46 +2,21 @@ import boto3
 import boto3.dynamodb.types as dynamodb_types
 import random
 
-# from dynamo_ctl import DynamoCtl, set_attr
 from util import iso_formatted_date_today
 
 dynamo = boto3.client('dynamodb')
 
 
 class User:
-    def __init__(self, alexa_user_id, attr, when=''):
-        # 記録
-        if attr.get('last_launch_date', '') == iso_formatted_date_today:
-            # 今日すでに情報を持っていたら取りたい
-            today_attr = attr['when'][iso_formatted_date_today]
-            self.follower_increase: int = today_attr.get('follower_increase', 0)
-            self.destination: str = today_attr.get('destination', '')
-
-        # 永続
+    def __init__(self, alexa_user_id, attr):
         self.alexa_user_id: str = alexa_user_id
         self.follower_total_amount: int = attr.get('follower_total_amount', 0)
         self.last_launch_date: str = attr.get('last_launch_date', '')
-        self._when = when
-
-    def __del__(self):
-        print('del')
-        print(self.__dict__)
-        d = {}
-        for k, v in self.__dict__.items():
-            if v:
-                d[k] = v
-        # set_user(self.alexa_user_id, d)
-
-    @property
-    def when_attr(self):
-        return {
-            'follower_increase': self.follower_increase,
-            'destination': self.destination
-            }
+        self.follower_increase = attr.get('follower_increase', 0)
 
     @property
     def attributes(self) -> dict:
-        return {'attr': self.__dict__}
+        return self.__dict__
 
     @property
     def is_first_launch_today(self) -> bool:
@@ -62,17 +37,3 @@ def serialize_attribute(attributes):
 
 def get_user(alexa_user_id, attr) -> User:
     return User(alexa_user_id, attr)
-
-
-# def set_user(alexa_user_id, user_attr):
-#     attr = {
-#         'when': {
-#             iso_formatted_date_today: {
-#                 }
-#             },
-#         'last_launch_date': iso_formatted_date_today,
-#         'follower_total_amount': user_attr['follower_total_amount']
-#         }
-#     for attribute, value in user_attr.items():
-#         attr['when'][iso_formatted_date_today][attribute] = value
-#     set_attr(alexa_user_id, attr)
