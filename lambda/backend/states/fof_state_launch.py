08bacba752e00ab223c6ba81fed7847d05fb3b72
emu_yuu <mhk2.747.vclv@gmail.com>
2020-04-06 21:11:50 +0900

    #card-34303232 create action

--- lambda/backend/states/fof_state_launch.py
+++ lambda/backend/states/fof_state_launch.py
@@ -6,26 +6,25 @@ from fof_sdk.dynamo_ctl import DynamoCtl
 
 def main(alexa_user_id):
     action = {'type': 'launch'}
-    response_text = [hero.message()]
+    response_texts = [hero.message()]
 
     with DynamoCtl(alexa_user_id) as dynamo_ctl:
         _user = user.get_user(alexa_user_id, dynamo_ctl.attr)
 
         if _user.is_first_launch_today:
             _user.increase_follower()
-            response_text.append(hero.increase_follower(
+            response_texts.append(hero.increase_follower(
                 _user.follower_increase, _user.follower_total_amount))
 
         if not _user.has_todays_oracle:
-            action = {
-                'type': 'ask_oracle',
-                'text': response_text.append(hero.ask_oracle())
-                }
+            response_texts.append(hero.ask_oracle())
+            action = {'type': 'ask_oracle'}
         dynamo_ctl.attr = _user.attr
 
     # post process
     # response['response_text'] = ''.join(response_text)
 
+    action['response_text'] = ''.join(response_texts)
     return action
 
 
