cbf16da18eb297208d3ed82b00686df5a285e537
GenkiTech <shimazu.g.r@gmail.com>
2020-04-19 17:20:52 +0900

    信者の増加数をガチャっぽい感じで実装

--- lambda/backend/fof_sdk/util.py
+++ lambda/backend/fof_sdk/util.py
@@ -1,3 +1,4 @@
+import random
 import datetime
 
 
@@ -5,3 +6,16 @@ iso_formatted_date_today = datetime.date.today().isoformat()
 """
 YYYY-mm-dd
 """
+
+follower_table = {'SS': 10,
+                  'S': 9,
+                  'A': 8,
+                  'B': 7,
+                  'C': 6,
+                  'D': 5
+                  }
+
+
+def gacha():
+    return random.choices(['SS', 'S', 'A', 'B', 'C', 'D'],
+                          weights=[0.1, 0.5, 5, 20, 30, 44.4])[0]
