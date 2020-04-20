838eb6f635340ef260aeee3ef0f1ff6ef9264fb0
emu_yuu <mhk2.747.vclv@gmail.com>
2020-04-20 21:44:59 +0900

    存在しない村には行けないようにする

--- lambda/backend/states/fof_state_oracle.py
+++ lambda/backend/states/fof_state_oracle.py
@@ -3,10 +3,11 @@ from __future__ import print_function
 from fof_sdk import hero
 from fof_sdk.dynamo_ctl import DynamoCtl
 from fof_sdk import user
+from fof_sdk import util
 
 
 def main(destination, alexa_user_id, ask_oracle_text):
-    if not destination:
+    if not util.is_valid_destination(destination):
         return {'type': 'oracle',
                 'response_text': f'大変申し訳ございません。わたくしの理解が及ばず、、、もう一度お告げを頂戴したく存じます。{ask_oracle_text}',
                 'set_should_end_session': False}
@@ -18,6 +19,7 @@ def main(destination, alexa_user_id, ask_oracle_text):
     with DynamoCtl(alexa_user_id) as dynamo_ctl:
         _user = user.get_user(alexa_user_id, dynamo_ctl.attr)
         _user.destination = destination
+
         print(f'user destination: {_user.destination}')
         dynamo_ctl.attr = _user.attr
 
