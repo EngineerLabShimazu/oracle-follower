36263409442c9c62dacec83b5c8b5f3f58f9deb6
emu_yuu <mhk2.747.vclv@gmail.com>
2020-04-28 20:31:38 +0900

    user.attrを保存

--- lambda/backend/fof_manual_handler.py
+++ lambda/backend/fof_manual_handler.py
@@ -11,6 +11,7 @@ def main(alexa_user_id, intent, destinations):
         with DynamoCtl(alexa_user_id) as dynamo_ctl:
             _user = user.get_user(alexa_user_id, dynamo_ctl.attr)
             follower_summary_recommend = f'現在の合計は{_user.follower_total_amount}人です。' if _user.follower_total_amount > 0 else ''
+            dynamo_ctl.attr = _user.attr
             return {
                 'type': 'help',
                 'response_text': '「お告げの勇者」では、この世界の神であるあなたのお告げによって勇者を成長させ、あなたの信者数を拡大していただきます。'
