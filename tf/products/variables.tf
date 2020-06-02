2a3fd176a2de59c41fe48b969cecadf1e534b1be
GenkiTech <shimazu.g.r@gmail.com>
2020-06-02 22:20:01 +0900

    feat: change fof backend stg arn

--- tf/products/variables.tf
+++ tf/products/variables.tf
@@ -1,7 +1,7 @@
 variable "env" {}
 variable "lambda_role" {}
 variable "lambda_environment_default" {}
-variable "backend_sfn_arn" {}
+variable "backend_sfn_arn_stg" {}
 variable "backend_sfn_arn_prd" {}
 variable "event_source_token" {}
 variable "sfn_role" {}
