3c537bf961ec629cd768e100c8be346743ad57c5
GenkiTech <shimazu.g.r@gmail.com>
2020-06-03 00:18:13 +0900

    feat: change fof backend stg arn

--- src/lambda/backend/fof_state_translator/fof_state_translator.py
+++ src/lambda/backend/fof_state_translator/fof_state_translator.py
@@ -2,14 +2,16 @@ from fof_sdk.dynamo_ctl import DynamoCtl
 from fof_sdk import user
 
 
-def main(alexa_user_id):
+def main(alexa_user_id, state):
     with DynamoCtl(alexa_user_id) as dynamo_ctl:
         _user = user.get_user(alexa_user_id, dynamo_ctl.attr)
         if _user.is_first_launch_skill():
             return 'tutorial'
+    return state
 
 
 def lambda_handler(event, context):
     alexa_user_id = event['alexa_user_id']
-    state = main(alexa_user_id)
+    _state = event['state']
+    state = main(alexa_user_id, _state)
     return state
