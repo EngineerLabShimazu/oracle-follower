8f649e7e31fb1a4ef441ff2572cd0ab885793b19
GenkiTech <shimazu.g.r@gmail.com>
2020-06-04 21:34:22 +0900

    feat: Tutorial State一通り通るように変更

--- src/lambda/backend/fof_state_tutorial/fof_state_tutorial.py
+++ src/lambda/backend/fof_state_tutorial/fof_state_tutorial.py
@@ -1,14 +1,21 @@
-def main():
+from nodes import node_map
+
+
+def main(node_key):
     action = {
         'type': 'tutorial',
         'set_should_end_session': False
         }
-    response_text = """
-    """
-    action['response_text'] = ''.join(response_text)
+    node = node_map.get(node_key)
+    action['response_text'] = ''.join(node['text'])
+    action['node'] = node.get('next_node', '')
+
+    if node.get('end'):
+        action['set_should_end_session'] = True
     return action
 
 
 def lambda_handler(event, context):
-    response = main()
-    return response
+    node_key = event.get('node', 'launch')
+    action = main(node_key)
+    return action
