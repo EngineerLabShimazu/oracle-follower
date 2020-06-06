c533892b8ef4adecd9d69f01213441df22dd3fc4
GenkiTech <shimazu.g.r@gmail.com>
2020-06-06 17:19:04 +0900

    feat: string に 対して capitalize したいので、 stateがstringであることを担保する

--- src/lambda/backend/fof_state_translator/fof_state_translator.py
+++ src/lambda/backend/fof_state_translator/fof_state_translator.py
@@ -2,7 +2,7 @@ from fof_sdk.dynamo_ctl import DynamoCtl
 from fof_sdk import user
 
 
-def main(alexa_user_id, state):
+def main(alexa_user_id, state: str) -> str:
     with DynamoCtl(alexa_user_id) as dynamo_ctl:
         _user = user.get_user(alexa_user_id, dynamo_ctl.attr)
         if _user.is_first_launch_skill():
@@ -12,6 +12,6 @@ def main(alexa_user_id, state):
 
 def lambda_handler(event, context):
     alexa_user_id = event['alexa_user_id']
-    _state = event.get('state')
+    _state = event.get('state', '')
     state = main(alexa_user_id, _state)
     return state.capitalize()
