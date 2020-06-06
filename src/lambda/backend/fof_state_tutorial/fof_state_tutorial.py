2eccd35414d9fbc72b5629ed7c5ae199f6e3c580
GenkiTech <shimazu.g.r@gmail.com>
2020-06-06 17:19:58 +0900

    feat: tutorialが終わってから、初回起動判定がFalseになるように変更する

--- src/lambda/backend/fof_state_tutorial/fof_state_tutorial.py
+++ src/lambda/backend/fof_state_tutorial/fof_state_tutorial.py
@@ -1,7 +1,9 @@
 from nodes import node_map
+from fof_sdk.dynamo_ctl import DynamoCtl
+from fof_sdk import user
 
 
-def main(node_key):
+def main(alexa_user_id, node_key):
     action = {
         'type': 'tutorial',
         'set_should_end_session': False
@@ -12,10 +14,17 @@ def main(node_key):
 
     if node.get('end'):
         action['set_should_end_session'] = True
+
+        with DynamoCtl(alexa_user_id) as dynamo_ctl:
+            # tutorial が終わったら、last_launch_date を入れ、
+            # skill 初回起動判定をFalseにする
+            _user = user.get_user(alexa_user_id, dynamo_ctl.attr)
+            dynamo_ctl.attr = _user.attr
     return action
 
 
 def lambda_handler(event, context):
+    alexa_user_id = event['alexa_user_id']
     node_key = event.get('node', 'launch')
-    action = main(node_key)
+    action = main(alexa_user_id, node_key)
     return action
