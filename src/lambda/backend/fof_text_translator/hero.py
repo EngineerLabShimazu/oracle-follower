3d70d87db05c0526d3b75b2d56ca0fc6944c20e0
GenkiTech <shimazu.g.r@gmail.com>
2020-06-09 15:16:05 +0900

    feat: hero の 話者 を 選択

--- src/lambda/backend/fof_text_translator/hero.py
+++ src/lambda/backend/fof_text_translator/hero.py
@@ -1,22 +1,38 @@
 text_map = {
     'HERO_MESSAGE': """
-    こんにちは神様。本日も {appreciate} 、ありがとうございます。
+    <voice name="Takumi">
+        こんにちは神様。本日も {appreciate} 、ありがとうございます。
+    </voice>
     """,
     'HERO_ACTION_REPORT': """
-    昨日は {destination} へ行き、 {monster} を討伐してまいりました！
+    <voice name="Takumi">
+        昨日は {destination} へ行き、 {monster} を討伐してまいりました！
+    </voice>
     """,
     'HERO_INCREASE_FOLLOWER': """
-    その結果、神様を信仰させていただきたいと申す者共が新たに{follower_increase}人増えました。
+    <voice name="Takumi">
+        その結果、神様を信仰させていただきたいと申す者共が新たに{follower_increase}人増えました。
+    </voice>
     """,
-    'HERO_TOTAL_FOLLOWERS': '現在の合計は{total_follower}人です。',
+    'HERO_TOTAL_FOLLOWERS': """
+    <voice name="Takumi">
+        現在の合計は{total_follower}人です。
+    </voice>
+    """,
     'HERO_ASK_ORACLE': """
-    本日は、 {destination_a} と {destination_b} のどちらへ向かえばよろしいでしょうか？
+    <voice name="Takumi">
+        本日は、 {destination_a} と {destination_b} のどちらへ向かえばよろしいでしょうか？
+    </voice>
     """,
     'HERO_REPEAT_ORACLE': """
-    {destination}へ行くのですね！ありがとうございます！また明日、結果を報告します！
+    <voice name="Takumi">
+        {destination}へ行くのですね！ありがとうございます！また明日、結果を報告します！
+    </voice>
     """,
     'HERO_PARDON': """
-    大変申し訳ございません。わたくしの理解が及ばず、、、。もう一度お告げを頂戴したく存じます。
+    <voice name="Takumi">
+        大変申し訳ございません。わたくしの理解が及ばず、、、。もう一度お告げを頂戴したく存じます。
+    </voice>
     """,
     'HERO_APPRECIATE_1': '見守ってくださって',
     'HERO_APPRECIATE_2': 'お気にかけてくださって',
