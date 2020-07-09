cc5248307a8e89c3ca0512558ce0fdefa9ebe2b1
GenkiTech <shimazu.g.r@gmail.com>
2020-07-09 19:45:11 +0900

    feat: user_attrがあればSaveAttrする

--- src/lambda/backend/fof_post_save_attr/lambda_function.py
+++ src/lambda/backend/fof_post_save_attr/lambda_function.py
@@ -2,5 +2,7 @@ from fof_sdk.dynamo_ctl import DynamoCtl
 
 
 def lambda_handler(event, _):
-    with DynamoCtl(event['alexa_user_id']) as dynamo_ctl:
-        dynamo_ctl.attr = event['user_attr']
+    user_attr = event.get('user_attr')
+    if user_attr:
+        with DynamoCtl(event['alexa_user_id']) as dynamo_ctl:
+            dynamo_ctl.attr = user_attr
