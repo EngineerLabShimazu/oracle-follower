ddc664c3d5bd80cfed601cba6073100889093bee
GenkiTech <shimazu.g.r@gmail.com>
2020-07-31 18:07:51 +0900

    feat: Delete Help from manual handler

--- src/lambda/backend/fof_manual_handler/lambda_function.py
+++ src/lambda/backend/fof_manual_handler/lambda_function.py
@@ -5,26 +5,11 @@ from fof_sdk import hero
 from fof_sdk import util
 
 
-def main(alexa_user_id, intent, destinations, product_reference_name):
+def main(alexa_user_id, intent, product_reference_name):
     action = {
         'type': 'end'
-        }
-    if intent == 'HelpIntent':
-        with DynamoCtl(alexa_user_id) as dynamo_ctl:
-            _user = user.get_user(alexa_user_id, dynamo_ctl.attr)
-            dynamo_ctl.attr = _user.attr
-
-            if not destinations:
-                destinations = random.sample(util.get_village_names(), 2)
-            return {
-                'type': 'help',
-                'original_texts': [
-                    {'text': 'SKILL_SUMMARY'},
-                    hero.total_followers(_user.follower_total_amount),
-                    hero.ask_oracle(destinations)
-                    ]
-                }
-    elif intent == 'CancelOrStopIntent':
+    }
+    if intent == 'CancelOrStopIntent':
         return {
             'type': 'cancel_or_stop',
             'image_url': util.get_image('hero/hero_anticipation'),
@@ -91,8 +76,6 @@ def main(alexa_user_id, intent, destinations, product_reference_name):
 def lambda_handler(event, context):
     alexa_user_id = event['alexa_user_id']
     intent = event['intent']
-    destinations = event.get('destinations_choice')
     product_reference_name = event.get('product_reference_name')
-    response = main(alexa_user_id, intent, destinations,
-                    product_reference_name)
+    response = main(alexa_user_id, intent, product_reference_name)
     return response
