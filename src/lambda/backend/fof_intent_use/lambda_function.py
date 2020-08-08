3a0aff36b1f6dff81d51973af07fff69af465258
emu_yuu <mhk2.747.vclv@gmail.com>
2020-08-08 12:41:07 +0900

    「クロノス チケットを使いますか？」「いいえ」でスキルを終了する

--- src/lambda/backend/fof_intent_use/lambda_function.py
+++ src/lambda/backend/fof_intent_use/lambda_function.py
@@ -1,8 +1,28 @@
 import intent_use_nodes as nodes
 from fof_sdk.user import User
+from fof_sdk import hero
+from fof_sdk import util
 
 
 def main(node, user, intent):
+    if intent == 'AMAZON.NoIntent':
+        return {
+            'type': 'cancel_or_stop',
+            'image_url': util.get_image('hero/hero_anticipation'),
+            'bg_image_url': util.get_image(
+                'bg/fof-map-gauss2', extension='.jpg'),
+            'original_texts': [
+                {
+                    'text': 'APPRECIATE_ON_STOP',
+                    'kwargs': {
+                        'appreciate': hero.get_appreciate_message()
+                    }
+                },
+                {
+                    'text': 'PLEASE_AGAIN_ON_STOP'
+                }
+            ]
+        }
     if intent not in ['AMAZON.YesIntent', 'UseIntent']:
         return nodes.re_ask()
     _node_map = {
--- src/lambda/frontend/fof_alexa_frontend/with_context_handler.py
+++ src/lambda/frontend/fof_alexa_frontend/with_context_handler.py
@@ -90,33 +90,17 @@ class WithContextIntentHandler(AbstractRequestHandler):
         speech_text = response["response_text"]
 
         image_url = response.get('image_url')
-        bg_image_url = response.get('bg_image_url')
-        image_title = response.get('image_title')
-        image_text = response.get('image_text')
         if image_url:
-            img_obj = Image(sources=[ImageInstance(url=image_url)])
-            bg_img_obj = Image(sources=[ImageInstance(url=bg_image_url)])
-            if util.is_support_display(handler_input):
-                handler_input.response_builder.add_directive(
-                    RenderTemplateDirective(
-                        BodyTemplate7(
-                            back_button=BackButtonBehavior.VISIBLE,
-                            image=img_obj,
-                            background_image=bg_img_obj,
-                            title=image_title)
+            handler_input.response_builder.set_card(
+                ui.StandardCard(
+                    title='title sample',
+                    text='text sample',
+                    image=ui.Image(
+                        small_image_url=image_url,
+                        large_image_url=image_url
                     )
                 )
-            else:
-                handler_input.response_builder.set_card(
-                    ui.StandardCard(
-                        title=image_title,
-                        text=image_text,
-                        image=ui.Image(
-                            small_image_url=image_url,
-                            large_image_url=image_url
-                        )
-                    )
-                )
+            )
 
         handler_input.response_builder.speak(speech_text).ask(
             speech_text).set_should_end_session(
