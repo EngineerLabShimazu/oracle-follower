153c0a0f8c71b3136a6b4301b5c226175551e75d
emu_yuu <mhk2.747.vclv@gmail.com>
2020-08-27 18:07:23 +0900

    FOF-37 feat: 想定外のインテントがきたら聞き返す

---
+++ src/lambda/backend/fof_intent_what_have_i_got/nodes.py
@@ -0,0 +1,17 @@
+def re_ask(node):
+    if node == 'ask_ganesha':
+        return {
+            'original_texts': [
+                {
+                    'text': 'ASK_GANESHA',
+                }
+            ]
+        }
+    elif node == 'ask_gem_pack':
+        return {
+            'original_texts': [
+                {
+                    'text': 'ASK_GEM_PACK',
+                }
+            ]
+        }
